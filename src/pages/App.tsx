8b18c34af0e3fbad829e2de0eb0cc0d3d5bc3c06
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-31 19:11:47 -0300

    refactor: move App component to pages directory and update imports path

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../components/Form/index"
+import List from "../components/List";
+
+function App() {
+  return <div className="App">
+    <Form/>
+    <List/>
+  </div>;
+}
+
+export default App;
