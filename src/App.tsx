cf816cae4bc93994befd874f7b074c650117c9d9
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-31 16:52:49 -0300

    feat: add list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Form from "./components/Form/index"
+import List from "./components/List";
 
 function App() {
   return <div className="App">
     <Form/>
+    <List/>
   </div>;
 }
 
---
+++ src/components/List/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function List() {
+    return(
+        <aside>
+            <h2> Estudos do dia </h2>
+            <ul>
+                <li>
+                    <h3> React </h3>
+                    <span>02:00:00</span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
