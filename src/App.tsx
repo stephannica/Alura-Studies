000646a9853eff49692e201302aa165c91b37d71
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-31 15:49:43 -0300

    feat: add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Button from "./components/Button"
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <Button/>
+  </div>;
 }
 
 export default App;
