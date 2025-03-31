2080232830118c92250be53d1657b06050ac45f8
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-31 18:13:05 -0300

    fix: add key prop to list items for improved rendering

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -16,7 +16,7 @@ function List() {
       <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li>
+          <li key={index}>
             <h3> {item.tarefa} </h3>
             <span> {item.tempo} </span>
           </li>
