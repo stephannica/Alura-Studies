a0971c2e0a86df63ed4ca394a22283bf3af7ce3a
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-31 18:12:05 -0300

    refactor: update List component to use map with static tasks

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 function List() {
+  const tarefas = [
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Typescript",
+      tempo: "01:00:00",
+    },
+  ];
   return (
     <aside>
       <h2> Estudos do dia </h2>
       <ul>
-        <li>
-          <h3> React </h3>
-          <span>02:00:00</span>
-        </li>
+        {tarefas.map((item, index) => (
+          <li>
+            <h3> {item.tarefa} </h3>
+            <span> {item.tempo} </span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
