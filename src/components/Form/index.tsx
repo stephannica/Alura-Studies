9774a3da7ced4d587c2c6933b06ce3bf84e9d512
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-02 16:35:52 -0300

    feat: ensure tasks and time can be added and submitted

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,15 +3,27 @@ import Button from "../Button";
 import style from "./Form.module.scss"
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00"
+  }
+
+  addTask(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    console.log('state: ', this.state)
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">Matéria</label>
           <input
             type="text"
             id="task"
             name="task"
+            value={this.state.task}
+            onChange={event => this.setState({...this.state, task: event.target.value})}
             placeholder="O que você gostaria de estudar?"
             required
           ></input>
@@ -24,6 +36,8 @@ class Form extends React.Component {
             id="time"
             name="time"
             step="1"
+            value={this.state.time}
+            onChange={event => this.setState({...this.state, time: event.target.value})}
             min="00:00:00"
             max="08:00:00"
             required
