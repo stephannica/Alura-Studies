f8cc7734fec81daa7e86ac5270ae74901d9f5376
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-02 11:15:31 -0300

    feat: add Stopwatch component to App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Form from "../components/Form/index"
 import List from "../components/List";
 import style from './App.module.scss'
+import Stopwatch from "../components/Stopwatch";
 
 function App() {
   return <div className={style.AppStyle}>
     <Form/>
+    <Stopwatch/>
     <List/>
   </div>;
 }
