c69cc2ea22a32789150d3f6fe2cb369e716c3e5b
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-03 14:55:26 -0300

    refactor: restructure List component to accept tasks prop

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,24 +1,11 @@
-import React, { useState } from "react";
 import style from './List.module.scss'
 import Item from "./Item";
+import { ITask } from '../../types/task';
 
-export default function List() {
-  const [tasks, setTasks] = useState([{
-    task: 'React',
-    time: '02:00:00'
-}, {
-    task: 'JavaScript',
-    time: '01:00:00'
-}, { 
-    task: 'Typescript',
-    time: '03:00:00'
-}]);;
-
+export default function List({tasks}: {tasks: ITask[]}) {
   return (
     <aside className={style.taskList}>
-      <h2 onClick={() => {
-        setTasks([...tasks, {task: "Estudar estado", time: "05:00:00"}])
-      }}> Estudos do dia </h2>
+      <h2> Estudos do dia </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item
