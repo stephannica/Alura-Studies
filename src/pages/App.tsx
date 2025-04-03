2b6313f1ad5c2c1a0c59954c397589b27b7f4018
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-03 14:51:38 -0300

    feat: integrate task state management

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,19 @@
-import React from "react";
-import Form from "../components/Form/index"
+import { useState } from "react";
+import Form from "../components/Form/index";
 import List from "../components/List";
-import style from './App.module.scss'
+import style from "./App.module.scss";
 import Stopwatch from "../components/Stopwatch";
+import { ITask } from "../types/task";
 
 function App() {
-  return <div className={style.AppStyle}>
-    <Form/>
-    <Stopwatch/>
-    <List/>
-  </div>;
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  return (
+    <div className={style.AppStyle}>
+      <Form setTasks={setTasks} />
+      <Stopwatch />
+      <List tasks={tasks} />
+    </div>
+  );
 }
 
 export default App;
