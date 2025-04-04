603628e5e614e8153e0d06b572dc8e0df88dff63
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-04 15:01:26 -0300

    feat: update Item component to handle task selection and reflect selected state in App component

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -5,10 +5,18 @@ interface ItemProps extends ITask{
   selectTask: (selectedTask: ITask) => void
 }
 
-export default function Item({ task, time, selected, completed, id }: ItemProps) {
+export default function Item({ task, time, selected, completed, id, selectTask }: ItemProps) {
   console.log('item atual: ', { task, time, selected, completed, id })
   return (
-    <li className={style.item}>
+    <li className={`${style.item} ${selected ? style.selectedItem : ''}`}
+    onClick={() => selectTask({
+      task,
+      time,
+      selected,
+      completed,
+      id
+    })}
+    >
       <h3> {task} </h3>
       <span> {time} </span>
     </li>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -11,6 +11,12 @@ function App() {
 
   function selectTask(selectedTask: ITask) {
     setSelected(selectedTask);
+    setTasks((oldTasks) =>
+      oldTasks.map((task) => ({
+        ...task,
+        selected: task.id === selectedTask.id ? true : false
+      }))
+    );
   }
 
   return (
