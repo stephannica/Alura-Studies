a0c9a7fb1ecde370df938853ad999585834e25a8
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-07 14:58:45 -0300

    feat: add countdown functionality

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -16,14 +16,23 @@ export default function Stopwatch({selected} : StopwatchProps) {
         if(selected?.time) {
             setTime(timeToSeconds(selected.time));
         }
-    }, [selected])
+    }, [selected]);
+
+    function countdown(time: number = 0) {
+        setTimeout(() => {
+            if(time > 0) {
+                setTime(time - 1);
+                return countdown(time - 1);
+            }
+        }, 1000)
+    }
     return(
         <div className={style.stopwatch}>
             <p className={style.title}>Escolha um card e inicie o cronômetro</p>
             <div className={style.clockWrapper}>
                 <Clock time={time}/>
             </div>
-            <Button>
+            <Button onClick={() => countdown(time)}>
                 Começar!
             </Button>
         </div>
