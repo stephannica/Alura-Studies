a3d4133cba1276b04b8df235df469bb4d1e3c51f
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-07 15:33:37 -0300

    refactor: convert class component to function component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import style from './Button.module.scss';
+import React from "react";
+import style from "./Button.module.scss";
 
-class Button extends React.Component<{
-  children: React.ReactNode,
-  type?: "button" | "submit" | "reset" | undefined, onClick?: () => void
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.button}>
-        {this.props.children}
-      </button>
-    )
-  }
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+function Button({type = 'button', onClick, children}: ButtonProps) {
+  return (
+    <button onClick={onClick} type={type} className={style.button}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
